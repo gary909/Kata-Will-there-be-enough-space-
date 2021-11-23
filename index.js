951b2c1d24965b8fbc466b674139346fb982ab36
gary <gary.white@lodgit.com>
2021-11-23 13:22:48 +0100

    code working

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function enough(cap, on, wait) {
-    // your code here
+    if (cap - on >= wait) {
+        return 0;
+    } else {
+        return wait - (cap - on);
+    }
 }
 
 console.log(enough(10, 5, 5)); // Return 0
 console.log(enough(100, 60, 50)); // Return 10
 console.log(enough(20, 5, 5)); // Return 0
+console.log(enough(72, 38, 39)); // Return 5
