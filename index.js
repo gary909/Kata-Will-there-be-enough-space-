0dbb7b455621253a0e8f110bdf64466b0a1e07df
gary <gary.white@lodgit.com>
2021-11-23 13:06:16 +0100

    boilerplate

---
+++ index.js
@@ -0,0 +1,7 @@
+function enough(cap, on, wait) {
+    // your code here
+}
+
+console.log(enough(10, 5, 5)); // Return 0
+console.log(enough(100, 60, 50)); // Return 10
+console.log(enough(20, 5, 5)); // Return 0
